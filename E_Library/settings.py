3797f403989da482cd805334a360e51a25be919d
Imran Abu Libda <110631034+3m0r9@users.noreply.github.com>
2023-04-07 01:41:10 +0800

    Update settings.py

--- E_Library/settings.py
+++ E_Library/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'imranabulibda@gmail,com'
-EMAIL_HOST_PASSWORD = 'jmCkF0sazjzooTBPucx8b5lV4XjIeM11'
+EMAIL_HOST_USER = 'imranabulibda@gmail.com'
+EMAIL_HOST_PASSWORD = 'ppoourdqvtflpftx'
