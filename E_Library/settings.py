2712ff69b69571d0b6827e57fa96b80518a1f585
3m0r9 <110631034+3m0r9@users.noreply.github.com>
2023-04-07 17:39:38 +0800

    2nd

--- E_Library/settings.py
+++ E_Library/settings.py
@@ -137,9 +137,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'imranabulibda@gmail,com'
-EMAIL_HOST_PASSWORD = 'jmCkF0sazjzooTBPucx8b5lV4XjIeM11'
+EMAIL_HOST_USER = 'imranabulibda@gmail.com'
+EMAIL_HOST_PASSWORD = 'zuxrchlrweypnfbq'
+
